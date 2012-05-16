72c3d7f22ebda837466aeb1ada3b528a4f7431bb
rayosun <saritalocita@gmail.com>
2012-05-16 19:46:09 -0400

    didn't know how to print it as an array so I just printed each line and it was like NOUFAIL

--- Sara/src/teach/solve_these/GenerateFibonacci.java
+++ Sara/src/teach/solve_these/GenerateFibonacci.java
@@ -10,8 +10,22 @@ public class GenerateFibonacci extends NoneToIntArrTest {
    }
 
    public int[] test() {
-       //Return an integer array of the first 10 fibonnacci numbers
-      // Generate them yourself, don't hardcode them!
+      
+   int lastnum = 1;
+   int nextnum = 0;
+   int thisnum = 0;
+   
+   
+       for (int i = 0; i < 10; i++){
+           thisnum = lastnum + nextnum;
+           lastnum = nextnum;
+           nextnum = thisnum;
+           System.out.println(thisnum);
+       
+       }
+       
+       
+       
        return null;
    }
 }
