1428fc49523e3dbe1663abd197871990bf89e01f
Carrotman42 <kevin@chowski.com>
2012-05-17 19:00:29 -0400

    Added two new questions to solve.

---
+++ Sara/src/teach/kevin_reference_solutions/VariableWrapInParenthesis.java
@@ -0,0 +1,22 @@
+package teach.kevin_reference_solutions;
+
+import teach.testrig.testTypes.StringIntToStringTest;
+
+public class VariableWrapInParenthesis extends StringIntToStringTest {
+   
+   public boolean testSolution() {
+      return true;
+   }
+   
+   public String test(String arg1, int arg2) {
+      // Return a string that is the provided String that is preceded by a number of left parenthesis
+      //   and succeeded by the same number of right parenthesis. This number is given to you in
+      //   arg2. Example:
+      //
+      //   arg1 == "pp", arg2 == 4
+      //
+      //   "((((pp))))" should be returned.
+      return null;
+   }
+   
+}
---
+++ Sara/src/teach/kevin_reference_solutions/WrapInParenthesis.java
@@ -0,0 +1,17 @@
+package teach.kevin_reference_solutions;
+
+import teach.testrig.testTypes.StringToStringTest;
+
+public class WrapInParenthesis extends StringToStringTest {
+   
+   public boolean testSolution() {
+      return true;
+   }
+   
+   public String test(String arg1) {
+      // Return a string that is the provided String that is preceded by a left parenthesis
+      //   and succeeded by a right parenthesis
+      return null;
+   }
+   
+}
