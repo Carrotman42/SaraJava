938a3a02d69fecd993843b56674d08f46feb4c77
Carrotman42 <kevin@chowski.com>
2012-05-16 22:54:01 -0400

    Gave a hint on the problem.

--- Sara/src/teach/solve_these/GenerateFibonacci.java
+++ Sara/src/teach/solve_these/GenerateFibonacci.java
@@ -10,11 +10,10 @@ public class GenerateFibonacci extends NoneToIntArrTest {
    }
 
    public int[] test() {
-      
-   int lastnum = 1;
-   int nextnum = 0;
-   int thisnum = 0;
-  // int[] answer = new int[10];
+      int lastnum = 1;
+      int nextnum = 0;
+      int thisnum = 0;
+      // int[] answer = new int[10];
    
    
    
@@ -27,10 +26,32 @@ public class GenerateFibonacci extends NoneToIntArrTest {
        //    System.out.println(answer[i]);
          //  return answer;
            
-           }
+           //}
        }
        
+       // You're very close! The thing to remember (I'm not sure if I even said this before) is
+       //   that when a 'return' is executed, then the function is immediately stopped and control
+       //   returns to where the method was called. For example:
        
+       /*
+        * public static void main(String[] args) {
+        *    System.out.println(returnExample());
+        * }
+        * 
+        * public static int returnExample() {
+        *    for (int i = 0; i < 10; i++) {
+        *       return i;
+        *    }
+        * }
+        */
+       
+       // Will always print out '0' because in the first time the loop is run (during its first iteration)
+       //   the program encounters a return 'statement' and immediately returns. No further things will be
+       //   run in that method once a return happens.
+       
+       // Therefore, the strategy here is to calculate all of the fibonacci numbers that you need to in the
+       //   loop and then after the loop is finished (after the ending curly brace of the for loop) return
+       //   array that you created.
        
        return null;
    }
